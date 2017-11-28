1d6fcde20d362e5b367d2e7184e937b7642d2ad6
Nikolai Shabalin <dev.nikolai.shabalin@gmail.com>
2017-11-28 13:04:19 +0300

    revert js changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,20 @@
 'use strict';
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
   // Открываем и закрываем меню в мобильной версиии
   var mobileMenuBox = document.querySelector('.mobile-menu-box');
-  var body = document.body;
-
-  document.querySelector('.open-menu-btn').onclick = function (event) {
-    event.preventDefault();
+  document.querySelector('.open-menu-btn').onclick = function (e) {
+    e.preventDefault();
     mobileMenuBox.classList.remove('close');
     mobileMenuBox.classList.add('open');
     mobileMenuBox.style.opacity = 100;
-    body.classList.add('mobile-menu-hidden');
+    document.body.classList.add('mobile-menu-hidden');
   };
 
-  document.querySelector('.close-menu-btn').onclick = function (event) {
-    event.preventDefault();
+  document.querySelector('.close-menu-btn').onclick = function (e) {
     mobileMenuBox.style.opacity = 0;
     mobileMenuBox.classList.remove('open');
     mobileMenuBox.classList.add('close');
-    body.classList.remove('mobile-menu-hidden');
+    document.body.classList.remove('mobile-menu-hidden');
+    e.preventDefault();
   };
 });
