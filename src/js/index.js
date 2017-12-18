e24eb5a509fe7672f4f03a19b8437a48a8f41b99
Nikolay Shabalin <dev.nikolai.shabalin@gmail.com>
2017-12-18 15:50:40 +0300

    #19 - подключение скритов переносит в конец документа. Удаляет событие DOMContentLoaded, так как оно уже произошло. Уменьшает обращениt в DOM в index.js (#53)

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,20 @@
 'use strict';
-document.addEventListener('DOMContentLoaded', function () {
-  // Открываем и закрываем меню в мобильной версиии
-  var mobileMenuBox = document.querySelector('.mobile-menu-box');
-  var body = document.body;
+// Открываем и закрываем меню в мобильной версиии
+var body = document.body;
+var mobileMenuBox = body.querySelector('.mobile-menu-box');
 
-  document.querySelector('.open-menu-btn').onclick = function (event) {
-    event.preventDefault();
-    mobileMenuBox.classList.remove('close');
-    mobileMenuBox.classList.add('open');
-    mobileMenuBox.style.opacity = 100;
-    body.classList.add('mobile-menu-hidden');
-  };
+body.querySelector('.open-menu-btn').onclick = function (event) {
+  event.preventDefault();
+  mobileMenuBox.classList.remove('close');
+  mobileMenuBox.classList.add('open');
+  mobileMenuBox.style.opacity = 100;
+  body.classList.add('mobile-menu-hidden');
+};
 
-  document.querySelector('.close-menu-btn').onclick = function (event) {
-    event.preventDefault();
-    mobileMenuBox.style.opacity = 0;
-    mobileMenuBox.classList.remove('open');
-    mobileMenuBox.classList.add('close');
-    body.classList.remove('mobile-menu-hidden');
-  };
-});
+body.querySelector('.close-menu-btn').onclick = function (event) {
+  event.preventDefault();
+  mobileMenuBox.style.opacity = 0;
+  mobileMenuBox.classList.remove('open');
+  mobileMenuBox.classList.add('close');
+  body.classList.remove('mobile-menu-hidden');
+};
